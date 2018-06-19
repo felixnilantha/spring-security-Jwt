ebe9d325a6f5b7b0e14507958f361b160749c975
Nilantha Silva <felixnilantha@gmail.com>
2018-06-19 16:12:37 +1000

    init commit

---
+++ src/main/java/com/consciousprogramming/user/ApplicationUserRepository.java
@@ -0,0 +1,8 @@
+package com.consciousprogramming.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ApplicationUserRepository extends JpaRepository<ApplicationUser, Long> {
+
+	ApplicationUser findByUsername(String username);
+}
